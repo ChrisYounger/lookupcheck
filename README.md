9984b201fe9e6ba57967878ab0d939c5652e664a
ChrisYounger <chris.younger@gmail.com>
2024-08-06 19:14:50 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Description
+
+Analyse lookup files for common errors, such as:
+- blank column names
+- lines with an unexpected amount of columns
+- leading/trailing whitespace in column headings
+- does not check for large files but outputs the size so you can decide on your own limits
+
+
+
+# Usage: 
+
+- provide a field called "path" which is the full path to the CSV files to check
+- Will output fields:
+    - lookupcheck_status: the warnings and errors found with the file (or OK)
+    - lookupcheck_size: the size of the file in bytes
+
+
+
+# Example:
+
+```
+| rest /servicesNS/-/-/data/lookup-table-files 
+| rename eai:data as path
+| table path
+| lookupcheck
+| search NOT lookupcheck_status="OK"
+| table path lookupcheck_status lookupcheck_size
+```
+
+Copyright (C) 2024 Chris Younger | [Splunkbase](https://splunkbase.splunk.com/app/) | [Source code](https://github.com/ChrisYounger/lookupcheck) | [My Splunk apps](https://splunkbase.splunk.com/apps/#/author/chrisyoungerjds)
---
+++ bin/lookupcheck.py
@@ -0,0 +1,66 @@
+# Copyright (C) 2024 Chris Younger
+
+import os, csv
+import splunk.Intersplunk
+
+def lookupcheck():
+
+    results, dummyresults, settings = splunk.Intersplunk.getOrganizedResults()
+    
+    if len(results) == 0:
+        results = [{"lookupcheck_status": "ERROR provide a list of CSV file full paths in the \"path\" field"}]
+    else:
+        problem_limit = 5
+        for row in results:
+            problems = []
+            if not "path" in row:
+                problems.append("ERROR path field not found")
+            elif row["path"] == "":
+                problems.append("ERROR path field is blank")
+            else:
+                if not os.path.isfile(row["path"]):
+                    problems.append("ERROR file not found")
+                else:
+                    size = -1
+                    try:
+                        size = os.path.getsize(row["path"])
+                    except OSError:
+                        pass                
+                    row["lookupcheck_size"] = str(size)
+                    if not row["path"].lower().endswith('.csv'):
+                        problems.append(f"WARN no csv extension on file")
+                    with open(row["path"]) as csv_file:
+                        try:
+                            csv_reader = csv.reader(csv_file, delimiter=',')
+                            line_count = 0
+                            column_count = 0
+                            for csvrow in csv_reader:
+                                line_count += 1
+                                if line_count == 1:
+                                    column_count = len(csvrow)
+                                    # not outputting the row becuase this doesnt correctly do csv escaping
+                                    #row["lookupcheck_columns"] = ",".join(csvrow)
+                                    for column in csvrow:
+                                        column_trimmed = column.strip()
+                                        if column_trimmed == "":
+                                            problems.append(f"WARN blank column header field")
+                                        elif column_trimmed != column:
+                                            problems.append(f"ERROR column \"{column_trimmed}\" has leading or trailing whitespace")
+                                else:
+                                    if column_count != len(csvrow):
+                                        problems.append(f"ERROR line {line_count} has unexpected amount of columns")
+                                
+                        except Exception as ex:
+                            problems.append("ERROR unexpected exception: " + str(ex))
+            problem_count = len(problems)
+            if problem_count > problem_limit:
+                row["lookupcheck_status"] = "\n".join(problems[:(problem_limit-1)]) + f"\nNot showing {problem_count-problem_limit+1} more problems..."
+            elif problem_count:
+                row["lookupcheck_status"] = "\n".join(problems)
+            else:
+                row["lookupcheck_status"] = "OK"
+            
+    splunk.Intersplunk.outputResults(results)
+
+if __name__ == '__main__':
+    lookupcheck()
